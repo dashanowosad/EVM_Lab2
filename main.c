152883066a36b512966e28da18327e205ec23c80
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-04-24 23:21:58 +0700

    Add UI

--- main.c
+++ main.c
@@ -5,78 +5,59 @@
 #include <stdio.h>
 
 int main(){
-	int tmp, commandtest,c , o,i, j, z=0;
-	 int A[2] = {0x3033F00, 0x3F30303F}, u;
-	/*sc_memoryInit();
-	sc_regInit();
-	printf("Setting cell 30 value 45\n");
-	sc_memorySet(30, 45);
-	printf("Getting cell value 30 \n");
-	sc_memoryGet(30, &tmp);
-	printf("%d ", tmp);*/
-//	printf("\n Filling memory with an increasing sequence and writing to a file \n");
+	int tmp, i, j, z;
+	int A[2] = {0xFFC3C3FF, 0xFF030303};
 	mt_clrscr();
-//	mt_gotoXY(2,2);
+	z = 3;
+	mt_gotoXY(2, 2);
 	for(i = 0; i < 10; i++){
 		for(j = 0; j < 10;j++){
-			sc_memorySet(i, z);
+			sc_memorySet(i, 9999);
 			sc_memoryGet(i, &tmp);
 			printf("%4d ", tmp);
-			z++;
 		}
-		printf("\n");
+		mt_gotoXY(z, 2);
+		z++;
 	}
-	bc_box(1,1,11,51);
-	mt_gotoXY(1,23);
+	bc_box(1, 1, 11, 51);
+	mt_gotoXY(1, 23);
 	printf("Memory");
-	bc_box(12,1,21,47);
-	bc_printbigchar(A,13,2,0,7);
-	bc_printbigchar(A,13,11,0,7);
-	bc_printbigchar(A,13,20,0,7);
-	bc_printbigchar(A,13,29,0,7);
-	bc_printbigchar(A,13,38,0,7);
-	/*
-	sc_memorySave("Files/swap.bin");
-	printf("\n Reinitialize RAM and read it from file \n");
-	sc_memoryInit();
-	printf("Reinitialized RAM \n");
-	for(i = 0; i < N; i++){
-		sc_memoryGet(i, &tmp);
-                printf("%d ", tmp);
-	}
-	printf("\n RAM after reading from file \n");
-	sc_memoryLoad("Files/swap.bin");
-	for(i = 0; i < N; i++){
-		sc_memoryGet(i, &tmp);
-		printf("%d ", tmp);
-	}
-	printf("\n Checking the operation of the outgoing memory flag \n");
-	sc_memorySet(1213, 45);
-	sc_regGet(AOF, &tmp);
-	printf("Flag = %d", tmp);
-	sc_regSet(AOF, 0);
-	printf("\n Checking the command endecode function \n");
-	printf("%d ",sc_commandEncode(SUB, 99, &commandtest));
-	printf("\n Checking the command decode function \n");
-	sc_commandDecode(commandtest, &c, &o);
-	printf("\n Command = %d, Operand = %d \n", c, o);
-	sc_commandEncode(1, 234567890,&commandtest);
-	sc_commandDecode(commandtest, &c, &o);
-        printf("\n Command = %d, Operand = %d \n", c, o);
-	sc_commandEncode(2, 234567890,&commandtest);
-        sc_commandDecode(commandtest, &c, &o);
-        printf("\n Command = %d, Operand = %d \n", c, o);
-	
-
-	mt_clrscr();
-	bc_box(1,1,20,20);
-	bc_printbigchar(A, 2, 25, 0, 7);
-	bc_setbigcharpos(A, 0, 0, 0);
-	bc_printbigchar(A, 20, 25, 0, 7);
-	bc_getbigcharpos(A, 4, 0, &u);
-	printf("%d",u);
-	mt_setbgcolor(10);
-*/
+	bc_box(12, 1, 21, 46);
+	bc_box(1, 52, 3, 72);
+	mt_gotoXY(1, 57);
+	printf("Accumulator");
+	bc_box(4, 52, 6, 72);
+	mt_gotoXY(4, 54);
+        printf("InstructionCounter");
+	bc_box(7, 52, 9, 72);
+	mt_gotoXY(7, 58);
+        printf("Operation");
+	bc_box(10, 52, 12, 72);
+	mt_gotoXY(10, 60);
+        printf("Flags");
+	bc_box(13, 47, 21, 72);
+	mt_gotoXY(13, 48);
+        printf("Keys:");
+	mt_gotoXY(14, 48);
+	printf("l  - load");
+	mt_gotoXY(15, 48);
+	printf("s  - save");
+	mt_gotoXY(16, 48);
+	printf("r  - run");
+	mt_gotoXY(17, 48);
+	printf("t  - step");
+	mt_gotoXY(18, 48);
+	printf("i  - reset");
+	mt_gotoXY(19, 48);
+	printf("F5 - accumulator");
+	mt_gotoXY(20, 48);
+	printf("F6 - instructionCounter");
+	bc_printbigchar(A, 13, 2, 0, 7);
+        bc_printbigchar(A, 13, 11, 0, 7);
+        bc_printbigchar(A, 13, 20, 0, 7);
+        bc_printbigchar(A, 13, 29, 0, 7);
+        bc_printbigchar(A, 13, 38, 0, 7);
+	mt_gotoXY(22, 1);
 return 0;
 }
 
