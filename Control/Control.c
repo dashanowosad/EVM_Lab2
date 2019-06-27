f8b960cf81c724df0f4c4cd6605ddf369d8643cf
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-06-28 00:04:48 +0700

    Fix CU

--- Control/Control.c
+++ Control/Control.c
@@ -3,17 +3,16 @@
 
 int ALU(int command, int operand) {
 	int value;
-	if (sc_memoryGet(operand, &value) == -1)
-	{
+	if (sc_memoryGet(operand, &value) == -1){
 		sc_regSet(M, 1);
 		return -1;
 	}
-	switch(command)
-	{
+	switch(command){
 		case ADD:
 			AC += value;
-			if (Accumulator > 32767 || Accumulator < -32767)
+			if (Accumulator > 32767 || Accumulator < -32767){
 				return -1;
+			
 			break;
 		case SUB:
 			AC -= value;
@@ -41,7 +40,46 @@ int ALU(int command, int operand) {
 
 
 int CU(){
+	int operand, command, value;
+	sc_memoryGet(CR, &value);
+	if (!sc_commandDecode(value, &command, &operand)) {
+		sc_regSet(FlagIncorrectCommand, 1);
+		return -1;
+	}
+	if (command >= 0x30 && command <= 0x33){
+		if(ALU(command, operand) == 0)
+			return 0;
+		else
+			return -1;
+	}else{
+		switch(command){
+			case READ:{
+			}
+			case WRITE:{
+			}
+			case LOAD:{
+				sc_MemoryGet(operand, &value);
+				value &= 0x7FFF;
+				AC=value;
+				break;
+			}
+			case STORE:{
+				sc_MemorySet(operand, AC);
+				break;
+			}
+			case JUMP:{
+				CR=operand;
+			}	
+			case JNEG:{
+				if(AC<0)
+					CR=operand;
+
+			}
+			case JZ:{
+			}
+			case HALT:{
+			}
+
+	}
 	
 
-
-}
