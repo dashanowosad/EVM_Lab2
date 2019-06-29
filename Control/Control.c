8e18d7cc32b55a7a9dd2a58d5746c0b079256094
Тимур <nbvehrnbvehrnbvehr@gmail.com>
2019-06-30 00:24:50 +0700

    Fix

--- Control/Control.c
+++ Control/Control.c
@@ -69,7 +69,7 @@ int CU(){
 			}
 			case LOAD:{
 				sc_memoryGet(operand, &value);
-				value &= 0x3FFF;
+				value &= 0x7FFF;
 				AC=value;
 				CR++;
 				break;
@@ -107,7 +107,6 @@ int CU(){
 					AC = ((AC << operand) | (AC >> (31 - operand)) & (~(1 << 31)));
 				else
     				AC = ((AC << operand) | (AC >> (31 - operand)) | (1 << 31));
-					//AC = (AC << operand) | (unsigned()AC) >>  (32 - operand));
 					CR++;
 				break;
 			}
